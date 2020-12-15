6bbe04a877e0e3587f1d2cb75c9b1ff1a50bc6ad
CinisArboris <eyver.evm@gmail.com>
2020-12-15 00:57:28 -0400

    add : [test_cliente_03] enviando mensaje via SMTP

--- src/CLIENTE/ClienteSMTP.java
+++ src/CLIENTE/ClienteSMTP.java
@@ -2,6 +2,8 @@ package CLIENTE;
 
 import java.io.*;
 import java.net.*;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,9 +14,12 @@ import java.util.logging.Logger;
 public class ClienteSMTP {
     private String HOST;
     private int PUERTO;
+    private String cmd;
+    private String from;
+    private String to;
 
     public ClienteSMTP() {
-        this.HOST = "127.0.0.1";
+        this.HOST = "www.tecnoweb.org.bo";
         this.PUERTO = 25;
     }
     public ClienteSMTP(String HOST, int PUERTO) {
@@ -34,31 +39,122 @@ public class ClienteSMTP {
     public void setPUERTO(int PUERTO) {
         this.PUERTO = PUERTO;
     }
+    public String getCmd() {
+        return cmd;
+    }
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
     
     /**
      * Conexión simple al servidor.
      */
     void test_cliente_01() {
         try {
-            Socket socCli = new Socket(
-                    this.getHOST(), this.getPUERTO()
-            );
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(socCli.getInputStream()));
+            Socket sok = new Socket(this.getHOST(), this.getPUERTO());
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(sok.getInputStream()));
+            
             System.out.println("[C]"+"Conectado a:"+this.getHOST());
             System.out.println("[S]"+entrada.readLine());
-            socCli.close();
+            
+            sok.close();
         } catch (IOException ex) {
             //Logger.getLogger(ClienteSMTP.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println("[System]ERROR 404");
         }
     }
     
+    /**
+     * Conexion de 3 sesiones.
+     */
+    private void test_cliente_02() {
+        System.out.println("NOT YET");
+    }
+
+    /**
+     * Conexion simple enviando mensaje.
+     */
+    private void test_cliente_03() {
+        try {
+            Socket sok = new Socket(this.getHOST(), this.getPUERTO());
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(sok.getInputStream()));
+            DataOutputStream salida = new DataOutputStream (sok.getOutputStream());
+            
+            // #Primer contacto.
+            System.err.println("[C]Conectando ..."+this.getHOST()+":"+this.getPUERTO());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Saludo al servidor.
+            this.setCmd("helo "+this.getHOST()+"\r\n");
+            salida.writeBytes(this.getCmd());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Asignar el correo emisor del mensaje.
+            this.setCmd("mail from: "+"eyver.evm@gmail.com"+"\r\n");
+            salida.writeBytes(this.getCmd());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Asignar el correo destino del mensaje.
+            this.setCmd("rcpt to: "+"grupo01sa@tecnoweb.org.bo"+"\r\n");
+            salida.writeBytes(this.getCmd());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Asignar los datos del mensaje.
+            this.setCmd("data"+"\r\n");
+            salida.writeBytes(this.getCmd());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Asignar el asunto del mensaje.
+            this.setCmd("subject: MSJ DESDE JAVA \r\n");
+            System.out.println(this.getCmd());
+            salida.writeBytes(this.getCmd());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Asignar el mensaje del mensaje.
+            this.setCmd(
+                    "HOLA HOLA TECNO WEB"+"\r\n"+
+                    "Mensaje nocturno desde java"+"\r\n"
+                );
+            System.out.println(this.getCmd());
+            salida.writeBytes(this.getCmd());
+            TimeUnit.SECONDS.sleep(1);
+            
+            // #Fin del mensaje.
+            this.setCmd("."+"\r\n");
+            salida.writeBytes(this.getCmd());
+            System.out.println("[S]"+entrada.readLine());
+            TimeUnit.SECONDS.sleep(1);
+            
+            System.err.println("[C]Cerrando sesión.");
+            sok.close();
+            entrada.close();
+            salida.close();
+        } catch (IOException ex) {
+            //Logger.getLogger(ClienteSMTP.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("[System]ERROR 404");
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ClienteSMTP.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("[System]ERROR SLEEP");
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
-        // Conexion simple
-        ClienteSMTP cli = new ClienteSMTP("127.0.0.1", 25);
-        cli.test_cliente_01();
+        // #Inicializacion de los [CLIENTES].
+        /* A */ //ClienteSMTP cli = new ClienteSMTP("127.0.0.1", 25);
+        /* B */ ClienteSMTP cli = new ClienteSMTP();//B
         
-        // Conexion para 3
+        // #Conexion simple.
+        //cli.test_cliente_01();
         
+        // #Conexion simple 3 sesiones paralelas.
+        //cli.test_cliente_02();
+        
+        // #Conexion simple enviar mensaje.
+        cli.test_cliente_03();
     }
 }
