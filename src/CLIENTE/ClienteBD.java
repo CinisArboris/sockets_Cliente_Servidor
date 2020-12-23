30491ad80e6e2c4c9d8aca906700b3fae0c1255f
CinisArboris <eyver.evm@gmail.com>
2020-12-22 22:50:14 -0400

    update : Clase [ClienteDB] con métodos separados.

--- src/CLIENTE/ClienteBD.java
+++ src/CLIENTE/ClienteBD.java
@@ -1,13 +1,10 @@
 package CLIENTE;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
 
 /**
  *
@@ -17,23 +14,24 @@ public class ClienteBD {
     private String HOST;
     private String PORT;
     private String BD;
+    private String TBL;
     private String USR;
     private String PWD;
-    private String TBL;
-    
+    private Properties NAV;
+    private Connection CNX;
 
     public ClienteBD() {
-        this.HOST   = "www.#.org.bo";
-        this.PORT   = "#";
-        this.USR    = "#";
-        this.PWD    = "#";
-        this.BD     = "#";
-        this.TBL    = "#";
+        this.HOST = "localhost";
+        this.PORT = "5432";
+        this.BD = "db_agenda";
+        this.TBL = "amigo";
+        this.USR = "default";
+        this.PWD = "default";
+        this.NAV = new Properties();
+        this.CNX = null;
     }
-    public ClienteBD(String HOST, String PORT) {
-        this.HOST = HOST;
-        this.PORT = PORT;
-    }
+    
+    
 
     public String getHOST() {
         return HOST;
@@ -53,6 +51,12 @@ public class ClienteBD {
     public void setBD(String BD) {
         this.BD = BD;
     }
+    public String getTBL() {
+        return TBL;
+    }
+    public void setTBL(String TBL) {
+        this.TBL = TBL;
+    }
     public String getUSR() {
         return USR;
     }
@@ -65,33 +69,59 @@ public class ClienteBD {
     public void setPWD(String PWD) {
         this.PWD = PWD;
     }
-    public String getTBL() {
-        return TBL;
+    public Properties getNAV() {
+        return NAV;
     }
-    public void setTBL(String TBL) {
-        this.TBL = TBL;
+    public void setNAV(String option, String value) {
+        this.NAV.setProperty(option, value);
     }
-    
-    
-    public static void main(String[] args) {
-        ClienteBD cli = new ClienteBD();
-        cli.conectar();
+    public Connection getCNX() {
+        return CNX;
     }
-
-    private void conectar() {
+    public void setCNX(String url, Properties nav) {
         try {
-            Properties nav = new Properties();
-            nav.setProperty("user", this.getUSR());
-            nav.setProperty("password", this.getPWD());
-            nav.setProperty("ssl", "True");
-            
-            String urlBD = "jdbc:postgresql://"+this.getHOST()+":"+this.getPORT()+"/"+this.getBD();
-            
-            Connection con = DriverManager.getConnection(urlBD, nav);
-            
+            this.CNX = DriverManager.getConnection(url, nav);
         } catch (SQLException ex) {
-            Logger.getLogger(ClienteBD.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(ClienteBD.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("ERROR - setCNX");
         }
     }
     
+    private void conectar() {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("[USR] ");
+        this.setUSR(input.nextLine());
+            
+        System.out.print("[PWD] ");
+        this.setPWD(input.nextLine());
+        
+        System.out.println(this.getUSR() + this.getPWD());
+        this.setNAV("user", this.getUSR());
+        this.setNAV("password", this.getPWD());
+        this.setNAV("ssl", "false");
+        
+        String urlBD = "jdbc:postgresql://"+
+                            this.getHOST()+":"+
+                            this.getPORT()+"/"+
+                            this.getBD();
+        this.setCNX(urlBD, this.getNAV());
+        System.out.println("200 - CONECTADO");
+    }
+    
+    private void desconectar() {
+        try {
+            this.CNX.close();
+            System.out.println("200 - DESCONECTADO");
+        } catch (SQLException ex) {
+            //Logger.getLogger(ClienteBD.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("ERROR - desconectar");
+        }
+    }
+    public static void main(String[] args) {
+        ClienteBD cli = new ClienteBD();
+        cli.conectar();
+        cli.desconectar();
+        
+    }
 }
